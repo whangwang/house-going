8de7a44e4c0057fa635df7a3191fcab6c255d461
whangwang <whangwang0430@gmail.com>
2017-12-19 05:23:41 +0800

    test payload

--- index.js
+++ index.js
@@ -177,7 +177,8 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('message sent!');
+      console.log(response);
     } else {
       console.error("Unable to send message:" + err);
     }
