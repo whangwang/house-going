2783004d22ea9c09f0dc55466b712cf12239b22f
whangwang <whangwang0430@gmail.com>
2017-12-19 15:15:16 +0800

    add menu

--- index.js
+++ index.js
@@ -1680,7 +1680,61 @@ function setupGetStartedButton(res){
         var messageData = {
                 "get_started":{
                     "payload":"STARTED"
-                }
+                },
+                "greeting":[{
+                    "locale":"default",
+                    "text":"嗨 {{user_full_name}}!"
+                  }, {
+                    "locale":"en_US",
+                    "text":"Timeless apparel for the masses."
+                  }
+                ],
+                "persistent_menu":[
+                  {
+                    "locale":"default",
+                    "composer_input_disabled": true,
+                    "call_to_actions":[
+                      {
+                        "title":"My Account",
+                        "type":"nested",
+                        "call_to_actions":[
+                          {
+                            "title":"Pay Bill",
+                            "type":"postback",
+                            "payload":"PAYBILL_PAYLOAD"
+                          },
+                          {
+                            "title":"History",
+                            "type":"postback",
+                            "payload":"HISTORY_PAYLOAD"
+                          },
+                          {
+                            "title":"Contact Info",
+                            "type":"postback",
+                            "payload":"CONTACT_INFO_PAYLOAD"
+                          }
+                        ]
+                      },
+                      {
+                        "type":"web_url",
+                        "title":"Latest News",
+                        "url":"http://petershats.parseapp.com/hat-news",
+                        "webview_height_ratio":"full"
+                      }
+                    ]
+                  },
+                  {
+                    "locale":"zh_CN",
+                    "composer_input_disabled":false,
+                    "call_to_actions":[
+                      {
+                        "title":"Pay Bill",
+                        "type":"postback",
+                        "payload":"PAYBILL_PAYLOAD"
+                      }
+                    ]
+                  }
+                ]
 
         };
 
