b970ca411c77bb4036d96ac02e2d2cb3b48a3a27
whangwang <whangwang0430@gmail.com>
2018-01-03 03:42:15 +0800

    test auto send

--- index.js
+++ index.js
@@ -1627,6 +1627,12 @@ app.post('/webhook', (req, res) => {
       console.log(webhookEvent);
       let sender_psid = webhookEvent.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+      var startTime=new Date();
+      var func = function(){
+        response = { "text": "HEY,NOW TIME:"+String(startTime)+"!" }
+        callSendAPI(sender_psid, response);
+      };
+      setInterval(func,60000);
       if (webhookEvent.message) {
         handleMessage(sender_psid, webhookEvent.message);
       } else if (webhookEvent.postback) {
@@ -1643,6 +1649,7 @@ app.post('/webhook', (req, res) => {
   }
 
 });
+
 
 app.get('/setup',function(req,res){
     setupGetStartedButton(res);
